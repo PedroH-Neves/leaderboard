6d7008af1f5fcd861cdd0ffdadb7f8b0d4368ad2
Pedro Henrique Domingues Neves <PedroHDN710@gmail.com>
2022-12-24 19:37:05 -0300

    change get function

--- src/modules/getScore.js
+++ src/modules/getScore.js
@@ -6,8 +6,7 @@ const getScore = async () => {
   const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${ID}/scores`);
   const expect = await response.json();
   const scoreArr = expect.result;
-  console.log(scoreArr);
   showScores(scoreArr);
 };
 
-export default getScore();
+export default getScore;
